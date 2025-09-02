981232e642bab43b395a9dd8060c82436a48e38d
michael.hurtado@utp.edu.co <michael.hurtado.edu.co>
2025-09-01 20:44:37 -0500

    Se guardan cambios para combinar ramas

--- src/views/Header.js
+++ src/views/Header.js
@@ -1,27 +1,35 @@
 // src/views/Header.js
 function Header() {
   return (
-    <header class="header">
-      <div class="header-top">
-        <img src="public/assets/logo-agosto-header.png" alt="Alkosto Logo" class="logo" />
-        <nav class="nav-main">
-          <ul class="nav-list">
-            <li><a href="#">Hiperofertas</a></li>
-            <li><a href="#">Venta: (601) 746 8001</a></li>
-            <li><a href="#">Servicio: (601) 407 3033</a></li>
-            <li><a href="#">Sigue tu pedido</a></li>
-            <li><a href="#">Consulta tu factura</a></li>
-            <li><a href="#">Nuestras tiendas</a></li>
-            <li><a href="#">Catálogo</a></li>
-            <li><a href="#">Ayuda</a></li>
-          </ul>
-          <ul class="nav-user">
-            <li><a href="#">Mi cuenta</a></li>
-            <li><a href="#">Mi carrito</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+    <>
+      <a className="preheader-events" href="/ofertas">
+        <div className="text-preheader">
+          <p className="only-desktop"><strong>¡Aprovecha las ofertas más esperadas del año!</strong></p>
+          <p className="only-mobile"><strong>¡Las ofertas más esperadas del año!</strong></p>
+        </div>
+      </a>
+      <header className="header">
+        <div className="header-top">
+          <img src="assets/logo-agosto-header.png" alt="Alkosto Logo" className="logo" />
+          <nav className="nav-main">
+            <ul className="nav-list">
+              <li><a href="#">Hiperofertas</a></li>
+              <li><a href="#">Venta: (601) 746 8001</a></li>
+              <li><a href="#">Servicio: (601) 407 3033</a></li>
+              <li><a href="#">Sigue tu pedido</a></li>
+              <li><a href="#">Consulta tu factura</a></li>
+              <li><a href="#">Nuestras tiendas</a></li>
+              <li><a href="#">Catálogo</a></li>
+              <li><a href="#">Ayuda</a></li>
+            </ul>
+            <ul className="nav-user">
+              <li><a href="#">Mi cuenta</a></li>
+              <li><a href="#">Mi carrito</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
   );
 }
 
